bd8c87fe2e2a3a47e2d6d21df215fa8e52182a89
Lubos V. <tridosm@gmail.com>
2026-03-16 21:32:30 +0100

    Keep UDP port 7872

--- app/src/main/java/com/winlator/cmod/xr/XrVersion04.java
+++ app/src/main/java/com/winlator/cmod/xr/XrVersion04.java
@@ -24,10 +24,6 @@ public class XrVersion04 extends XrVersion03 {
         super();
     }
 
-    public int getPortIn() {
-        return 7728;
-    }
-
     @Override
     public int[] getPortsOut() {
         return new int[]{7872, 7873};
